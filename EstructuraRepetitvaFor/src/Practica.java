14d7b53bc1ce7265020294c3bfb7916edeb4a083
Giovanni Aleman <AlemanZ1998@gmail.com>
2024-04-15 08:31:02 -0600

    Proyecto finalizado

---
+++ EstructuraRepetitvaFor/src/Practica.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Practica {
+    public static void main(String[] args) {
+        
+    Scanner lector = new Scanner(System.in);
+
+    System.out.println("Ingese su nombre");
+    
+
+
+
+}
+}
